ca1ad9bab5808f8cf94eb78199ca1b03187c44c1
AbdulWahhab <info.awahhab@gmail.com>
2024-11-12 12:23:10 +0530

    globalmiddleware

--- src/middlewares/GlobalMiddleware.ts
+++ src/middlewares/GlobalMiddleware.ts
@@ -6,7 +6,6 @@ export class GlobalMiddleware {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       next(new Error(errors.array()[0].msg));
-      
     } else {
       next();
     }
